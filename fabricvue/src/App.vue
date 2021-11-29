58e646cfb20c78b6b46f1f58ad3065b26c01c397
TuanPham <tuanpham@rowboatsoftware.com>
2021-11-29 15:49:43 +0700

    install vue

---
+++ fabricvue/src/App.vue
@@ -0,0 +1,26 @@
+
+
+<template>
+  <CanVas />
+</template>
+<script lang="ts">
+import CanVas from './components/CanVas.vue'
+export default {
+  setup() {
+
+  },
+  components: {
+    CanVas,
+  },
+};
+</script>
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ fabricvue/src/components/CanVas.vue
@@ -0,0 +1,20 @@
+<template>
+    <h1>{{ h1 }}</h1>
+    <canvas ref="canVas" width="200" height="200"></canvas>
+</template>
+<script lang="ts">
+import { ref } from 'vue';
+import { fabric } from 'fabric';
+
+export default {
+    setup() {
+        const canVas = ref();
+        const h1 = ref<string>('CanVas To Fill');
+        const canvas = new fabric.Canvas(canVas);
+        console.log(canvas);
+        return {
+            h1,
+        };
+    },
+};
+</script>
