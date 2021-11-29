adc4dde9fee0dabe9de28161e7032bd5ec63671f
TuanPham <tuanpham@rowboatsoftware.com>
2021-11-29 17:39:11 +0700

    fix bug create canvas at file canvas.vue

--- fabricvue/src/components/CanVas.vue
+++ fabricvue/src/components/CanVas.vue
@@ -1,32 +1,32 @@
 <template>
     <h1>{{ h1 }}</h1>
-    <div ref="canVas">
-        <canvas  width="200" height="200"></canvas>
+    <div>
+        <canvas ref="canVasSelector" width="500" height="500"></canvas>
     </div>
 </template>
 <script lang="ts">
-import { ref } from 'vue';
+import { onMounted, ref } from 'vue';
 import { fabric } from 'fabric';
 
 export default {
     setup() {
         const canVasSelector = ref<HTMLDivElement>();
-        const canvasSelectorDetails = canVasSelector.value?.querySelector('canvas');
         const h1 = ref<string>('CanVas To Fill');
-        console.log(canvasSelectorDetails);
-        const canvas = new fabric.Canvas(canvasSelectorDetails);
-        const rect = new fabric.Rect({
-            fill: 'red',
-            width: 20,
-            height: 20
+        onMounted(() => {
+            const canvas = new fabric.Canvas(canVasSelector.value);
+            const rect = new fabric.Rect({
+                fill: 'red',
+                width: 50,
+                height: 50,
+                top: 10,
+                left: 10,
+            });
+            canvas.add(rect);
+            canvas.renderAll();
         });
-        canvas.add(rect);
         return {
             canVasSelector,
             h1,
-            canvasSelectorDetails,
-            canvas,
-            rect,
         };
     },
 };
