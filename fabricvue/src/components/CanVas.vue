27d753eef9012481300647c6cccf313c3cc8caaf
TuanPham <tuanpham@rowboatsoftware.com>
2021-12-01 17:21:06 +0700

    build version vue, create function add shape and add text drop drag

--- fabricvue/src/components/CanVas.vue
+++ fabricvue/src/components/CanVas.vue
@@ -4,14 +4,18 @@
     <div class="wrap d-flex justify-content-center">
         <canvas ref="canVasSelector" width="500" height="500"></canvas>
         <ControlMenu
+        :hasOpenRemove="isRemoveObj"
         @value-opacity="pickValueOpacity"
         @value-scale="pickValueScale"
         @value-blur="pickValueBlur"
+        @add-text-canvas="pickValueText"
+        @delete-obj-select="deleteObj"
+        @create-shape="createShape"
         />
     </div>
 </template>
 <script lang="ts">
-import { onMounted, ref } from 'vue';
+import { onMounted, ref, watch } from 'vue';
 import { fabric } from 'fabric';
 import  ControlMenu  from './ControlMenu.vue';
 
@@ -24,6 +28,9 @@ export default {
         const h1 = ref<string>('CanVas To Fill');
         const valueOpacity = ref<number>(1.0);
         const canvasValue = ref();
+        const checkActionObj = ref(false);
+        const isRemoveObj = ref(true);
+        const objTarget = ref();
         const rect = new fabric.Rect({
             fill: 'rgba(255, 0, 0, 1.0)',
             width: 150,
@@ -38,7 +45,6 @@ export default {
             obj.set({
                 opacity: valueOpacity.value,
             });
-            console.log(obj);
             canvasValue.value.renderAll();
         };
         const pickValueScale = (value: number) => {
@@ -56,20 +62,109 @@ export default {
             });
             canvasValue.value.renderAll();
         }
+
+        const pickValueText = (value: string) => {
+            let createText = new fabric.Text(value, { left: 100, top: 100 });
+            canvasValue.value.add(createText);
+            canvasValue.value.renderAll();
+            checkActionObj.value = !checkActionObj.value;
+        }
+        const toggleButtonRemove = (value: any) => {
+            if(value) {
+                isRemoveObj.value = false;
+            } else {
+                isRemoveObj.value = true;
+            }
+        }
+        const deleteObj = () => {
+            canvasValue.value.remove(objTarget.value);
+            isRemoveObj.value = true;
+        }
+        const createShape = (value: string) => {
+            switch (value) {
+                case 'square':
+                    let square = new fabric.Rect({
+                        fill: 'blue',
+                        width: 150,
+                        height: 150,
+                        top: 10,
+                        left: 10,
+                    });
+                    console.log(square)
+                    canvasValue.value.add(square);
+                    canvasValue.value.renderAll();
+                    break;
+                case 'triangle':
+                    let triangle = new fabric.Triangle({
+                        fill: 'violet',
+                        width: 150,
+                        height: 150,
+                        top: 10,
+                        left: 10,
+                    });
+                    canvasValue.value.add(triangle);
+                    canvasValue.value.renderAll();
+                    break;
+                case 'circle':
+                    let circle = new fabric.Circle({
+                        left: 100,
+                        top: 100,
+                        radius: 50,
+                        fill:'#f39c12'
+                    });
+                    circle.dirty = true;
+                    console.log(circle);
+                    canvasValue.value.add(circle);
+                    canvasValue.value.renderAll();
+                    break;
+                case 'star':
+                    let polygon = new fabric.Polygon([
+                        {x: 200, y: 0},
+                        {x: 250, y: 50},
+                        {x: 250, y: 100},
+                        {x: 150, y: 100},
+                        {x: 150, y: 50} ], {
+                            left: 250,
+                            top: 150,
+                            angle: 0,
+                            fill: 'green'
+                        }
+                        );
+                    canvasValue.value.add(polygon);
+                    canvasValue.value.renderAll();
+                    break;
+            } 
+            
+        }
         onMounted(() => {
             const canvas = new fabric.Canvas(canVasSelector.value);
             canvasValue.value = canvas;
             canvasValue.value.add(rect);
             canvasValue.value.renderAll();
+            canvasValue.value.on('mouse:dblclick', (e: any) => {
+                toggleButtonRemove(e.target);
+                objTarget.value = e.target;
+            })
+        });
+
+        watch(() => checkActionObj.value, () => {
+            console.log(canvasValue.value);
+            console.log(canvasValue.value._objects);
         });
         
         return {
             canVasSelector,
             h1,
             valueOpacity,
+            isRemoveObj,
+            objTarget,
+
             pickValueOpacity,
             pickValueScale,
             pickValueBlur,
+            pickValueText,
+            deleteObj,
+            createShape,
         };
     },
 };
--- fabricvue/src/components/ControlMenu.vue
+++ fabricvue/src/components/ControlMenu.vue
@@ -1,7 +1,7 @@
 <template>
 <div class="menu-control d-flex flex-column card p-4">
     <h1 class="display-3">menu</h1>
-    <h3 class="text-start">opacity</h3>
+    <h3 class="text-uppercase text-start">opacity</h3>
     <a-row> 
         <a-col :span="12">
             <a-slider v-model:value="inputValue1" :min="0.1" :max="1.0" :step="0.1" @change="handleSlider" />
@@ -10,7 +10,7 @@
             <a-input-number v-model:value="inputValue1" :min="0.1" :max="1.0" :step="0.1" style="margin-left: 16px" @change="handleSlider"/>
         </a-col>
     </a-row>
-    <h3 class="text-start">scale</h3>
+    <h3 class="text-uppercase text-start">scale</h3>
     <a-row> 
         <a-col :span="12">
             <a-slider v-model:value="inputValue2" :min="50" :max="150" @change="handleSliderScale" />
@@ -19,7 +19,7 @@
             <a-input-number v-model:value="inputValue2" :min="50" :max="150" style="margin-left: 16px" @change="handleSliderScale"/>
         </a-col>
     </a-row>
-    <h3 class="text-start">blur</h3>
+    <h3 class="text-uppercase text-start">blur</h3>
     <a-row> 
         <a-col :span="12">
             <a-slider v-model:value="inputValue3" :min="1" :max="70" @change="handleSliderBlur" />
@@ -28,17 +28,55 @@
             <a-input-number v-model:value="inputValue3" :min="1" :max="70" style="margin-left: 16px" @change="handleSliderBlur"/>
         </a-col>
     </a-row>
+    <h3 class="text-uppercase text-start">add text</h3>
+     <a-row :gutter="[16,16]"> 
+        <a-col :span="12">
+            <a-input v-model:value="valueTextObj" placeholder="Basic usage" @keypress.enter="handleInputText" />
+         </a-col>
+        <a-col :span="4">
+            <a-button class="text-uppercase" type="primary" @click="handleInputText">add text</a-button>
+        </a-col>
+    </a-row>
+    <a-row class="mt-4" :gutter="[16,16]">
+        <a-col :span="24">
+            <a-button class="w-100 d-flex justify-content-center align-items-center" type="primary" @click="toggleListShape"> <span class="text-uppercase d-block mx-2">create shape</span> <i class="fas fa-shapes"></i></a-button>
+        </a-col>
+        <a-col :span="24" v-if="isShowShape">
+             <a-row>
+                <a-col :span="6" v-for="(itemn, index) in shapeList" :key="index">
+                    <a-button type="primary" @click="selectCreateShape(itemn.value)"><i class="fas" :class="itemn.shape"></i></a-button>
+                </a-col>
+             </a-row>
+        </a-col>
+        <a-col :span="24">
+            <a-button class="w-100" type="primary" danger :disabled="hasOpenRemove" @click="removeObj"><i class="fas fa-trash"></i></a-button>
+        </a-col>
+    </a-row>
 </div>
 </template>
 <script lang="ts">
-import { ref } from 'vue';
+import { ref, onMounted } from 'vue';
 
 export default {
+    props: {
+        hasOpenRemove: {
+            type: Boolean,
+            default: true
+        }
+    },
   setup(props: any, { emit }: any) {
     const inputValue = ref<number>(0);
     const inputValue1 = ref<number>(1);
     const inputValue2 = ref<number>(150);
     const inputValue3 = ref<number>(150);
+    const valueTextObj = ref<string>('');
+    const isShowShape = ref<boolean>(false);
+    const shapeList = ref<any[]>([
+        { shape: 'fa-square', value: 'square' },
+        { shape: 'fa-caret-up fs-3',  value: 'triangle' },
+        { shape: 'fa-circle',  value: 'circle' },
+        { shape: 'fa-star',  value: 'star' },
+    ]);
 
     const handleSlider = (value: number) => {
         inputValue.value = value;
@@ -54,15 +92,35 @@ export default {
       inputValue3.value = value;
       emit('value-blur', value);
     }
-
+    const handleInputText = () => {
+      emit('add-text-canvas', valueTextObj.value);
+      valueTextObj.value = '';
+    };
+    const removeObj = () => {
+         emit('delete-obj-select');
+    }
+    const toggleListShape = () => {
+        isShowShape.value = !isShowShape.value;
+    }
+    const selectCreateShape = (value: string) => {
+         emit('create-shape', value);
+    }
     return {
       inputValue,
       inputValue1,
       inputValue2,
       inputValue3,
+      valueTextObj,
+      isShowShape,
+      shapeList,
+
       handleSlider,
       handleSliderScale,
-      handleSliderBlur
+      handleSliderBlur,
+      handleInputText,
+      removeObj,
+      toggleListShape,
+      selectCreateShape,
     };
   },
 };
